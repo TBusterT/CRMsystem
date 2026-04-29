cac22d13c1806b772cb244f8faa02618b7381237
TBusterT <tarastsupak120@gmail.com>
2026-04-29 16:47:51 +0300

    com

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { ClientsModule } from './modules/clients/clients.module';
+import { FinanceModule } from './modules/finance/finance.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ClientsModule, FinanceModule, InventoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
